d5065d16dd31cf63af79552046a19883d10c23aa
sall0908 <sholehfaisal3@gmail.com>
2025-11-13 12:39:08 +0700

    Enforce HTTPS in production environment
    Force HTTPS scheme in production environment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,14 +1,21 @@
 <?php
+use Illuminate\Support\Facades\URL;
 
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+
+
+
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+   public function boot()
+{
+    if (config('app.env') === 'production') {
+       URL::forceScheme('https');
+    }
+}
     public function register(): void
     {
         //
