058daa5d196efb983e82c9ae59f166fc3ec5adc8
sall0908 <sholehfaisal3@gmail.com>
2025-11-13 12:43:18 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,26 +1,21 @@
 <?php
+
 namespace App\Providers;
+
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-   public function boot()
-{
-    if (config('app.env') === 'production') {
-       URL::forceScheme('https');
-    }
-}
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
